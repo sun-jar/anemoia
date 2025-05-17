bed8444413defad5f562b025b7f6bb138683f7b9
nabilmuafa <109450293+nabilmuafa@users.noreply.github.com>
2025-05-18 00:52:51 +0700

    feat: add room tone, make animation on continue, unfinished stage 2-3 transition

--- scripts/AudioManager.gd
+++ scripts/AudioManager.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var main_menu_theme = $MainMenuTheme
 @onready var shockwave_sfx = $ShockwaveSFX
+@onready var room_tone = $RoomToneSFX
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,3 +26,11 @@ func play_shockwave():
 func stop_shockwave():
 	shockwave_sfx.stop()
 	
+func play_room_tone():
+	room_tone.play()
+	var db_tween = create_tween()
+	db_tween.tween_property(room_tone, "volume_db", 1.0, 1.0)
+	
+func stop_room_tone():
+	room_tone.stop()
+	room_tone.volume_db = -50.0
