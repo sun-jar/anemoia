03841825328e45a8b6798c92a20c388c36833e58
nabilmuafa <109450293+nabilmuafa@users.noreply.github.com>
2025-05-22 06:42:02 +0700

    feat: use power source sprites

--- scripts/level/Stages.gd
+++ scripts/level/Stages.gd
@@ -3,9 +3,10 @@ extends TileMapLayer
 @export var stage: int
 @export var switches: Array
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if stage == 2:
+		$PowerSource.play("inactive_2")
 	if GameManager.player_stage >= stage:
 		for child in self.get_children():
 			if child.name in switches and not GameManager.closed_doors[child.id]:
